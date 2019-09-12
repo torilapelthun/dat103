0bf2291a8d7cf2c2892c6ee247f2a2f69a926556
hvl577499 <toril.sunde.apelthun'gmail.com>
2019-09-12 21:50:08 +0200

    Test2

--- oblig1/repeter.sh
+++ oblig1/repeter.sh
@@ -1,6 +1,11 @@
-#! /bin/bash
-declare -i i
-for((i=0;i<4;++i))
+#!/bin/bash
+#declare -i i
+#for((i=1;i=<$1;++i))
+#	do
+#	echo "Denne teksten skal repeteres"
+#done
+
+for((c=1; c<=$1; c++))
 	do
-	echo "Denne teksten skal repeteres"
+echo $2
 done
